98137afa35e494e364aec83e3ee04f99d7945826
kevan p <34320699+kevanpigott@users.noreply.github.com>
2020-04-12 12:24:22 -0400

    Add files via upload
    added files via upload

---
+++ BoidController2.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidController2 : MonoBehaviour
+{
+    public int numBoids;
+    public float minSpeed;
+    public float maxSpeed;
+    public float spredRange;
+    public float maxRange = 0;
+    public GameObject prefab_boid;
+    private GameObject centerMark;
+    public Camera cam;
+    private float X, Y, Z;
+    private GameObject[] boids;
+    private Vector3 centerOfFlock = new Vector3(0,0,0);
+    private Vector3 speedOfFlock;
+    private Vector3 rotationOfFlock;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // centerOfFlock = new Vector3(0, 0, 0);
+        centerMark = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        var RenderMark = centerMark.GetComponent<Renderer>();
+        RenderMark.material.SetColor("_Color", Color.red);
+
+        boids = new GameObject[numBoids];
+        for (int i = 0; i < numBoids; i++)
+        {
+            GameObject boid = Instantiate(prefab_boid) as GameObject;
+            X = Random.Range(-spredRange, spredRange) + boid.transform.position.x;
+            Y = Random.Range(-spredRange, spredRange) + boid.transform.position.y;
+            Z = Random.Range(-spredRange, spredRange) + boid.transform.position.z;
+            boid.transform.position = new Vector3(X, Y, Z);
+
+            RenderMark = boid.GetComponent<Renderer>();
+            RenderMark.material.SetColor("_Color", Color.black);
+
+            boid.GetComponent<boidSingle>().SetController(gameObject);
+
+            boids[i] = boid;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 sumPos = new Vector3(0, 0, 0);
+        Vector3 sumRot = new Vector3(0, 0, 0);
+        //Vector3 sumSpd = new Vector3(0, 0, 0);
+        for (int i = 0; i < numBoids; i++)
+        {
+            sumPos += boids[i].transform.position;
+            sumRot += new Vector3(boids[i].transform.rotation.x, boids[i].transform.rotation.y, boids[i].transform.rotation.z);
+            //sumSpd += boids[i].rigidbody.velocity;
+        }
+        centerOfFlock = sumPos / numBoids;
+        rotationOfFlock = sumRot / numBoids;
+        //speedOfFlock = sumSpd / numBoids;
+
+        cam.transform.LookAt(centerOfFlock);
+        centerMark.transform.position = centerOfFlock;
+    }
+
+    public Vector3 getCenter()
+    {
+        return centerOfFlock;
+    }
+    public Vector3 getRotation()
+    {
+        return rotationOfFlock;
+    }
+}
---
+++ boidSingle.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boidSingle : MonoBehaviour
+{
+    private GameObject Controller;
+    public float randTurnRangeMax;
+    public float randTurnRangeMin;
+    public float randTurnSpeedMin;
+    public float randTurnSpeedMax;
+    public float weight;
+    private BoidController2 MasterController;
+    private float minSpeed;
+    private float maxSpeed;
+    private float curSpeed;
+    private float curTurnSpeed;
+    private Vector3 rotVec;
+    private bool instantiated = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //forward
+        if (instantiated)
+        {
+            rotVec = getRotVec();
+            curTurnSpeed = Random.Range(randTurnSpeedMin, randTurnSpeedMax);
+            curSpeed = Random.Range(minSpeed, maxSpeed);
+
+            if (isInRange())
+            {
+                transform.Rotate(rotVec * curTurnSpeed * Time.deltaTime);
+            }
+            else
+            {
+                transform.LookAt(MasterController.getCenter());
+            }
+            transform.Translate(Vector3.forward * curSpeed * Time.deltaTime);
+        }
+
+    }
+    bool isInRange()
+    {
+        if (MasterController.maxRange == 0)
+        {
+            return true;
+        }
+        else
+        {
+
+            Vector3 distanceVec = (transform.position - MasterController.getCenter());
+            float distance = distanceVec.magnitude;
+
+            if (distance <= MasterController.maxRange) { return true; }
+            else { return false; }
+        }
+        
+    }
+    private Vector3 getRotVec()
+    {
+        float X, Y, Z, i, j, k, a, b, c;
+        i = Random.Range(randTurnRangeMin, randTurnRangeMax);
+        j = Random.Range(randTurnRangeMin, randTurnRangeMax);
+        k = Random.Range(randTurnRangeMin, randTurnRangeMax);
+
+        a = MasterController.getRotation().x;
+        b = MasterController.getRotation().y;
+        c = MasterController.getRotation().z;
+
+        if (weight != 0)
+        {
+            X = ((2 * (weight * rotVec.x + i)) + (2 * (1 - weight) * a)) / 2;
+            Y = ((2 * (weight * rotVec.y + j)) + (2 * (1 - weight) * b)) / 2;
+            Z = ((2 * (weight * rotVec.z + k)) + (2 * (1 - weight) * c)) / 2;
+        } else
+        {
+            X = rotVec.x + i;
+            Y = rotVec.y + j;
+            Z = rotVec.z + k;
+        }
+        return new Vector3(X, Y, Z);
+    }
+    public void SetController (GameObject Master)
+    {
+        Controller = Master;
+        MasterController = Controller.GetComponent<BoidController2>();
+        minSpeed = MasterController.minSpeed;
+        maxSpeed = MasterController.maxSpeed;
+        instantiated = true;
+    }
+    public Vector3 getRotationVector()
+    {
+        return rotVec;
+    }   
+}
